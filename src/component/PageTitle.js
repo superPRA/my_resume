cf2283aabe771943db02ec953912d41d2e78b63a
pooriya mosavi <super.pra2017@gmail.com>
2022-12-20 13:31:22 +0330

    fixing bugs

--- src/component/PageTitle.js
+++ src/component/PageTitle.js
@@ -1,17 +1,13 @@
 import React from "react";
 
 export default function PageTitle(prop) {
-  if (prop.withLine){
+  
     return (
-        <div className="flex justify-start items-center">
-          <h1 className="text-4xl font-semibold lg:font-bold dark:text-white">{prop.title}</h1>
-          <div className="bg-[#ef4060] w-64 h-[2px] mt-2 mx-6"></div>
+        <div className="flex justify-start items-center gap-x-2">
+          <h1 className="text-4xl font-semibold lg:font-bold dark:text-white0">{prop.title}</h1>
+          {prop.withLine && <div className="bg-[#ef4060] w-64 h-[2px] hidden md:block mt-2 mx-6"></div>}
         </div>
       );
-  }
-  return (
-    <div className="flex justify-start items-center">
-      <h1 className="text-4xl font-semibold lg:font-bold dark:text-white">{prop.title}</h1>
-    </div>
-  );
+  
+  
 }
